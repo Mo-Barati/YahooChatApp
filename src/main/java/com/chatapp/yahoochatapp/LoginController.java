80272e9150b2be60a3115afe9523bf22d188eee2
Mo-83 <m.barati83@hotmail.com>
2025-03-05 15:03:18 +0000

    some more updates

---
+++ src/main/java/com/chatapp/yahoochatapp/LoginController.java
@@ -0,0 +1,39 @@
+package com.chatapp.yahoochatapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button signupButton;
+
+    @FXML
+    private void initialize() {
+        loginButton.setOnAction(this::handleLogin);
+        signupButton.setOnAction(this::handleSignup);
+    }
+
+    @FXML
+    private void handleLogin(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        System.out.println("Login Attempt: " + username + " / " + password);
+    }
+
+    private void handleSignup(ActionEvent event) {
+        System.out.println("Redirecting to Signup...");
+        // Later, add signup screen navigation logic here
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.chatapp.yahoochatapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.chatapp.yahoochatapp to javafx.fxml;
